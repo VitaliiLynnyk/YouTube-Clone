81ee7d0e2c33fbc4213d054a2d4561f20f1c1760
Vitalii Lynnyk <vitaliilynnyk@Vitaliis-MacBook-Air.local>
2019-08-07 22:03:23 +0300

    Fixed eslint errors

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -9,23 +9,23 @@ const VideoDetail = ({ video }) => {
 
   return (
     <React.Fragment>
-      <iframe elevation={6} style={{ height: '70%' }} frameBorder="0"
+      <iframe
+        elevation={6}
+        style={{ height: '70%' }}
+        frameBorder="0"
         height="100%"
         width="100%"
         title="VideoPlayer"
-        src={videoSrc} />
+        src={videoSrc}
+      />
       <Paper elevation={6} style={{ padding: '15px' }}>
         <Typography variant="h4">
           {video.snippet.title} - {video.snippet.channelTitle}
         </Typography>
-        <Typography variant="subtitle1">
-          {video.snippet.channelTitle}
-        </Typography>
-        <Typography variant="subtitle2">
-          {video.snippet.description}
-        </Typography>
+        <Typography variant="subtitle1">{video.snippet.channelTitle}</Typography>
+        <Typography variant="subtitle2">{video.snippet.description}</Typography>
       </Paper>
-    </React.Fragment >
+    </React.Fragment>
   );
 };
 
